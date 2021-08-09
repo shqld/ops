0c92b5952a3521458da95e0a906af6ae0b71877b
Sho Miyamoto <me@shomiyamoto.dev>
2021-08-09 22:52:28 +0900

    Update commands/update-service

--- src/docker/update_service.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -eu
-
-cd /ops/src
-
-REPO=$REPO VERSION=$SERVICE_VERSION commands/sho/docker-pull
-IMAGE="ghcr.io/shqld/dev/app:$SERVICE_VERSION" NAME=$SERVICE_NAME commands/sho/docker-service-update
