a4f09654bf2129cbd84ed10228a73aaa0f7704ff
Sho Miyamoto <me@shqld.dev>
2022-02-02 19:01:52 +0900

    Add 'make help' for default goal

--- lib.mk
+++ lib.mk
@@ -6,3 +6,11 @@ MAKEFLAGS += --no-builtin-rules --no-buildint-variables --no-print-directory
 define touch
 	@mkdir -p $(dir $@); touch $@
 endef
+
+.DEFAULT_GOAL := help
+
+help: ## Self-documented Makefile
+	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) \
+		$(foreach val, $(MAKEFILE_LIST), | sed 's|$(val):||') \
+		| sort \
+		| awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
