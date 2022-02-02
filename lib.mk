258001f3b2cb6b02966ef9ed3e209dc8c36392b5
Sho Miyamoto <me@shqld.dev>
2022-02-02 18:54:16 +0900

    Override $(MAKEFLAGS) to disable unexpected behavior

--- lib.mk
+++ lib.mk
@@ -1,6 +1,8 @@
 OPS := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
 CUR := $(dir $(abspath $(firstword $(MAKEFILE_LIST))))
 
+MAKEFLAGS += --no-builtin-rules --no-buildint-variables --no-print-directory
+
 define touch
 	@mkdir -p $(dir $@); touch $@
 endef
